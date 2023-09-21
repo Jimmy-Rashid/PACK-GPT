c399edc7b3c4cd15cebef2d09b644898b2935515
Jimmy Rashid <jimmyrashid22@gmail.com>
2023-09-21 11:01:26 -0700

    Added media, starting doc loaders

--- server.js
+++ server.js
@@ -1,4 +1,14 @@
-import { config } from "dotenv"
-config()
+import { config } from "dotenv";
+config();
+console.log(process.env.API_KEY);
 
-console.log(process.env.API_KEY)
+import { TextLoader } from "langchain/document_loaders/fs/text";
+
+for (n = 1; n < 100; n++) {
+  window["Podcast " + n] = new TextLoader(
+    "Media/Podcast/Transcripts/" + n + ".txt"
+  );
+  window["Document " + n] = await ["Podcast " + n].load();
+
+  console.log(window["Podcast " + n]);
+}
