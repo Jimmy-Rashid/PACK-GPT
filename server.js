b7fe4af17f16801e689160043ef9729b2cd5b1ca
Jimmy Rashid <jimmyrashid22@gmail.com>
2023-09-21 15:50:31 -0700

    Working on pipe

--- server.js
+++ server.js
@@ -2,13 +2,43 @@ import { config } from "dotenv";
 config();
 console.log(process.env.API_KEY);
 
+import { ChatOpenAI } from "langchain/chat_models/openai";
 import { TextLoader } from "langchain/document_loaders/fs/text";
+import { ChatPromptTemplate } from "langchain/prompts";
+import { OpenAI } from "openai";
 
-for (n = 1; n < 100; n++) {
-  window["Podcast " + n] = new TextLoader(
-    "Media/Podcast/Transcripts/" + n + ".txt"
-  );
-  window["Document " + n] = await ["Podcast " + n].load();
+const llm = new OpenAI({
+  openAIApiKey: process.env.OPENAI_API_KEY,
+});
 
-  console.log(window["Podcast " + n]);
+const loaderArray = [];
+const docsArray = [];
+
+for (let n = 1; n < 1000; n++) {
+  try {
+    loaderArray.push(new TextLoader("Media/Podcast/Transcripts/" + n + ".txt"));
+    docsArray.push(await loaderArray[n - 1].load());
+    // console.log(loaderArray[n - 1]);
+  } catch (err) {
+    break;
+  }
 }
+
+const roboTemplate = `You are an adorable non-slaughtering member of humanity that takes in modular 
+  housing questions and answers them by parsing through the provided information, and relaying read 
+  phrases.`;
+
+const humanTemplate = "{ text }";
+
+const prompt = ChatPromptTemplate.fromMessages([
+  ["system", roboTemplate],
+  ["human", humanTemplate],
+]);
+
+const model = new ChatOpenAI({});
+
+const chain = prompt.pipe(model).pipe(docsArray);
+
+const result = await chain.invoke({
+  text: "how big is a module",
+});
