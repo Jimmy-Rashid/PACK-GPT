ff497648a6bef8c658c2fc29b1133653d16c4d28
Jimmy Rashid <jimmyrashid22@gmail.com>
2023-09-25 16:01:14 -0700

    💀

--- server.js
+++ server.js
@@ -2,6 +2,7 @@ import { config } from "dotenv";
 config();
 
 import { ChatOpenAI } from "langchain/chat_models/openai";
+import { DirectoryLoader } from "langchain/document_loaders/fs/directory";
 import { TextLoader } from "langchain/document_loaders/fs/text";
 import {
   ChatPromptTemplate,
@@ -9,43 +10,35 @@ import {
   HumanMessagePromptTemplate,
 } from "langchain/prompts";
 import { OpenAI } from "langchain/llms/openai";
-import { LLMChain } from "langchain/chains";
+import { LLMChain, RetrievalQAChain } from "langchain/chains";
+import { HNSWLib } from "langchain/vectorstores/hnswlib";
+import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import * as fs from "fs";
 
-const loaderArray = [];
-const docsArray = [];
+const loader = new DirectoryLoader("Media", {
+  ".txt": (path) => new TextLoader(path),
+});
 
-for (let n = 1; n < 1000; n++) {
-  try {
-    loaderArray.push(new TextLoader("Media/Podcast/Transcripts/" + n + ".txt"));
-    docsArray.push(await loaderArray[n - 1].load());
-    // console.log(loaderArray[n - 1]);
-  } catch (err) {
-    break;
-  }
-}
+const text = await loader.load();
+
+console.log(text);
+
+const textSplitter = new RecursiveCharacterTextSplitter({ chunkSize: 1000 });
+const docs = await textSplitter.splitText([text]);
+
+const vectorStorage = await HNSWLib.fromDocuments(docs, new OpenAIEmbeddings());
+
+const vectorRetriever = vectorStorage.asRetriever();
 
 const llm = new OpenAI({
-  temperature: 1,
+  temperature: 0,
 });
 
-const roboTemplate = `You are an adorable non-slaughtering member of humanity that takes in modular
-housing questions and answers them by parsing through the provided information, and relaying read
-phrases.`;
-
-const humanTemplate = "{text}";
-
-const promptTemplate = ChatPromptTemplate.fromMessages([
-  SystemMessagePromptTemplate.fromTemplate(roboTemplate),
-  HumanMessagePromptTemplate.fromTemplate(humanTemplate),
-]);
-
-const chain = new LLMChain({
-  prompt: promptTemplate,
-  llm: llm,
-});
+const chain = RetrievalQAChain.fromLLM(llm, vectorRetriever);
 
 const res = await chain.call({
-  text: "I am a home",
+  query: "how many home types are there?",
 });
 
 console.log(res);
