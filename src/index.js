4e40113910da3c5f9b792be2965965cd29a24a8d
Jimmy Rashid <jimmyrashid22@gmail.com>
2023-09-27 16:18:31 -0700

    Working slide up modal

--- src/index.js
+++ src/index.js
@@ -1,8 +1,22 @@
-const messageBox = document.getElementById('messageBox');
-const triggerButton = document.getElementById('triggerButton');
-const slideUpModal = document.getElementById('slideUpModal');
+const messageBox = document.getElementById("messageBox");
+const triggerButton = document.getElementById("triggerButton");
+const slideUpModal = document.getElementById("slideUpModal");
 
-triggerButton.addEventListener('click', () => {
-    localStorage.setItem('query', messageBox.value);
-    // console.log(localStorage.getItem('query'));
+triggerButton.addEventListener("click", () => {
+  localStorage.setItem("query", messageBox.value);
+  // console.log(localStorage.getItem('query'));
+
+  // For release / testing
+
+  //   (async () => {
+  //     if (messageBox.value.length >= 10) {
+  //       slideUpModal.classList.remove("translate-y-full");
+  //     } else if (messageBox.value.length > 0) {
+  //       alert("Please enter a more specific query!");
+  //     } else if (messageBox.value == "") {
+  //       alert("Please enter a query!");
+  //     }
+  //   })();
+
+  slideUpModal.classList.remove("translate-y-full");
 });
