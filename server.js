09150980a2441fa2b099c66a507f0123a51bceb4
Jimmy Rashid <jimmyrashid22@gmail.com>
2023-09-27 11:09:32 -0700

    Tailwind configured, working on navbar

--- server.js
+++ server.js
@@ -16,7 +16,12 @@ import { OpenAIEmbeddings } from "langchain/embeddings/openai";
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 import * as fs from "fs";
 
+// For podcasts and RDH
+// const text = fs.readFileSync("Media/CombinedText.txt", "utf8");
+
+// For Burnaby laneway housing guidelines
 const text = fs.readFileSync("Burnaby Media/Burnaby Text.txt", "utf8");
+
 const textSplitter = new RecursiveCharacterTextSplitter({ chunkSize: 1000 });
 const docs = await textSplitter.createDocuments([text]);
 
@@ -25,6 +30,8 @@ const vectorStorage = await HNSWLib.fromDocuments(docs, new OpenAIEmbeddings());
 const vectorRetriever = vectorStorage.asRetriever();
 
 const llm = new OpenAI({
+  cache: true,
+  maxConcurrency: 1,
   temperature: 0.5,
 });
 
@@ -32,7 +39,12 @@ const chain = RetrievalQAChain.fromLLM(llm, vectorRetriever);
 
 const res = await chain.call({
   query:
-    "What sort of rental revenue can i expect from renting a 1000 square foot laneway site in a lower value area, can you provide some samples based on general assumptions",
+    // "What sort of rental revenue can i expect from renting a 1000 square foot laneway site in a lower value area, can you provide some samples based on general assumptions",
+    // "How long does it take to get a building permit",
+    // "Are laneway homes expensive?",
+    // "Are laneway homes a good investement?",
+    // "What is the general opinion about modular laneway homes?",
+    "how large is a module?",
 });
 
 console.log(res);
---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {
+      colors: {
+        packgreen: {
+          dark: "#507e71",
+          light: "#9db9ad",
+        },
+      },
+    },
+  },
+  plugins: [],
+}
+
