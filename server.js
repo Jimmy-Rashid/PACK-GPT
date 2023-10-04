1badc8ae8e44f76f5c2c753b7b60ed5d2d51429d
Jimmy Rashid <jimmyrashid22@gmail.com>
2023-10-04 15:06:06 -0700

    Response entry, limiter

--- server.js
+++ server.js
@@ -1,20 +1,36 @@
 import { config } from "dotenv";
 config();
 
-import { ChatOpenAI } from "langchain/chat_models/openai";
-import { DirectoryLoader } from "langchain/document_loaders/fs/directory";
-import { TextLoader } from "langchain/document_loaders/fs/text";
-import {
-  ChatPromptTemplate,
-  SystemMessagePromptTemplate,
-  HumanMessagePromptTemplate,
-} from "langchain/prompts";
+// import { ChatOpenAI } from "langchain/chat_models/openai";
+// import { DirectoryLoader } from "langchain/document_loaders/fs/directory";
+// import { TextLoader } from "langchain/document_loaders/fs/text";
+// import {
+//   ChatPromptTemplate,
+//   SystemMessagePromptTemplate,
+//   HumanMessagePromptTemplate,
+// } from "langchain/prompts";
 import { OpenAI } from "langchain/llms/openai";
 import { LLMChain, RetrievalQAChain } from "langchain/chains";
 import { HNSWLib } from "langchain/vectorstores/hnswlib";
 import { OpenAIEmbeddings } from "langchain/embeddings/openai";
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import Bottleneck from "bottleneck";
 import * as fs from "fs";
+
+const limiter = new Bottleneck({
+  reservoir: 40,
+  reservoirIncreaseAmount: 1,
+  reservoirIncreaseInterval: 1000,
+  // For deployment
+  // ReservoirIncreaseMaximum: 20,
+  maxConcurrent: 5,
+  minTime: 200,
+});
+
+// const rateLimitedGetEmbedding = limiter.wrap(getEmbedding);
+// vectors = await Promise.all(
+//   documents.flat().map((doc) => rateLimitedGetEmbedding(doc))
+// );
 
 // For podcasts and RDH
 // const text = fs.readFileSync("Media/CombinedText.txt", "utf8");
@@ -43,7 +59,7 @@ const res = await chain.call({
     // "What is the general opinion about modular laneway homes?",
     "How long would it take me to get a building permit in Burnaby, BC?",
 
-    // Power apps, low coder
+  // Power apps, low coder
 });
 
 console.log(res);
