bfd6d1ee56d596d1e2b3061135217e748ac81cb4
Jimmy Rashid <jimmyrashid22@gmail.com>
2024-01-15 16:13:39 -0800

    working on compression

--- server.js
+++ server.js
@@ -24,42 +24,63 @@ import { ChatOpenAI } from "@langchain/openai";
 import { ChatPromptTemplate } from "@langchain/core/prompts";
 import { StringOutputParser } from "@langchain/core/output_parsers";
 
-import { HNSWLib } from "langchain/vectorstores/hnswlib";
+import { HNSWLib } from "@langchain/community/vectorstores/hnswlib";
+import { PDFLoader } from "langchain/document_loaders/fs/pdf";
 import { RetrievalQAChain } from "langchain/chains";
-import { OpenAIEmbeddings } from "langchain/embeddings/openai";
+import { OpenAIEmbeddings } from "@langchain/openai";
+import { EmbeddingsFilter } from "langchain/retrievers/document_compressors/embeddings_filter";
+// import { LLMChainExtractor } from "langchain/retrievers/document_compressors/chain_extract";
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
+import { ContextualCompressionRetriever } from "langchain/retrievers/contextual_compression";
 
 import * as fs from "fs";
 
-// For podcasts and RDH
-// const text = fs.readFileSync("Media/CombinedText.txt", "utf8");
-
-// For Burnaby laneway housing guidelines
-
-const text = fs.readFileSync("Burnaby Media/Burnaby Text.txt", "utf8");
-const textSplitter = new RecursiveCharacterTextSplitter({ chunkSize: 1000 });
-const docs = await textSplitter.createDocuments([text]);
-const vectorStorage = await HNSWLib.fromDocuments(docs, new OpenAIEmbeddings());
-const vectorRetriever = vectorStorage.asRetriever();
-
 const model = new ChatOpenAI({
-  // cache: true,
-  // maxConcurrency: 1,
-  // temperature: 0.5,
+  cache: true,
+  maxConcurrency: 1,
+  // temperature: 0.25,
 });
 
-const parser = new StringOutputParser({});
-
-const chain = RetrievalQAChain.fromLLM(model, vectorRetriever);
-
-const res = await chain.invoke({
-  query:
-    // "What sort of rental revenue can i expect from renting a 1000 square foot laneway site in a lower value area, can you provide some samples based on general assumptions",
-    // "How long does it take to get a building permit",
-    // "Are laneway homes expensive?",
-    "Are laneway homes a good investment?",
-    // "What is the general opinion about modular laneway homes?",
-    // "How long would it take me to get a building permit in Burnaby, BC?",
+const text = new PDFLoader("Burnaby Media/Burnaby Text.pdf", {
+  parsedItemSeparator: "",
+});
+const baseCompressor = new EmbeddingsFilter({
+  embeddings: new OpenAIEmbeddings(),
+  similarityThreshold: 0.8,
 });
 
-console.log(res.text);
+const docs = await text.load();
+
+const vectorStorage = await HNSWLib.fromDocuments(docs, new OpenAIEmbeddings());
+const vectorRetriever = new ContextualCompressionRetriever({
+  baseCompressor,
+  baseRetriever: vectorStorage.asRetriever(),
+});
+
+const retrievedDocs = await vectorRetriever.getRelevantDocuments(
+  "what is recommended area for a laneway home"
+);
+
+console.log({ retrievedDocs });
+
+// const parser = new StringOutputParser({});
+
+// const chain = RetrievalQAChain.fromLLM(model, vectorRetriever);
+
+// async function QueryParser(query) {
+//   const response = await chain.invoke({ query });
+//   return parser.parse(response.text);
+// }
+
+// const stream = await QueryParser.stream(
+//   // "What sort of rental revenue can i expect from renting a 1000 square foot laneway site in a lower value area, can you provide some samples based on general assumptions",
+//   // "How long does it take to get a building permit",
+//   // "Are laneway homes expensive?",
+//   "Are laneway homes a good investment?"
+//   // "What is the general opinion about modular laneway homes?",
+//   // "How long would it take me to get a building permit in Burnaby, BC?",
+// );
+
+// for await (const chunk of stream) {
+//   console.log(chunk);
+// }
