597a090d2f7c2f6077b8491ea1ddbb42f95e16c6
Jimmy Rashid <jimmyrashid22@gmail.com>
2024-01-17 13:36:15 -0800

    Working compression

--- server.js
+++ server.js
@@ -20,36 +20,51 @@ config();
 
 // GPT Configuration
 
-import { ChatOpenAI } from "@langchain/openai";
-import { ChatPromptTemplate } from "@langchain/core/prompts";
-import { StringOutputParser } from "@langchain/core/output_parsers";
-
 import { HNSWLib } from "@langchain/community/vectorstores/hnswlib";
 import { PDFLoader } from "langchain/document_loaders/fs/pdf";
+import { ChatOpenAI } from "@langchain/openai";
 import { RetrievalQAChain } from "langchain/chains";
 import { OpenAIEmbeddings } from "@langchain/openai";
 import { EmbeddingsFilter } from "langchain/retrievers/document_compressors/embeddings_filter";
-// import { LLMChainExtractor } from "langchain/retrievers/document_compressors/chain_extract";
+import { StringOutputParser } from "@langchain/core/output_parsers";
+import { DocumentCompressorPipeline } from "langchain/retrievers/document_compressors";
 import { RecursiveCharacterTextSplitter } from "langchain/text_splitter";
 import { ContextualCompressionRetriever } from "langchain/retrievers/contextual_compression";
 
-import * as fs from "fs";
-
 const model = new ChatOpenAI({
   cache: true,
+  maxTokens: 50,
+  modelName: "gpt-3.5-turbo",
   maxConcurrency: 1,
   // temperature: 0.25,
+  callbacks: [
+    {
+      handleLLMEnd(output) {
+        console.log(JSON.stringify(output.llmOutput, null, 2));
+      },
+    },
+  ],
 });
 
 const text = new PDFLoader("Burnaby Media/Burnaby Text.pdf", {
   parsedItemSeparator: "",
 });
-const baseCompressor = new EmbeddingsFilter({
-  embeddings: new OpenAIEmbeddings(),
-  similarityThreshold: 0.8,
-});
 
 const docs = await text.load();
+
+const embeddingsFilter = new EmbeddingsFilter({
+  embeddings: new OpenAIEmbeddings(),
+  similarityThreshold: 0.8,
+  k: 3,
+});
+const textSplitter = new RecursiveCharacterTextSplitter({
+  chunkSize: 300,
+  chunkOverlap: 0,
+});
+
+const baseCompressor = new DocumentCompressorPipeline({
+  transformers: [textSplitter, embeddingsFilter],
+});
 
 const vectorStorage = await HNSWLib.fromDocuments(docs, new OpenAIEmbeddings());
 const vectorRetriever = new ContextualCompressionRetriever({
@@ -57,30 +72,17 @@ const vectorRetriever = new ContextualCompressionRetriever({
   baseRetriever: vectorStorage.asRetriever(),
 });
 
-const retrievedDocs = await vectorRetriever.getRelevantDocuments(
-  "what is recommended area for a laneway home"
-);
+const query = "How big can I construct a laneway home";
 
-console.log({ retrievedDocs });
+const chain = RetrievalQAChain.fromLLM(model, vectorRetriever);
 
-// const parser = new StringOutputParser({});
+const message = await chain.invoke({
+  query: query + " Answer using atleast 30 words, but no more than 50 words.",
+});
 
-// const chain = RetrievalQAChain.fromLLM(model, vectorRetriever);
+console.log(message);
 
-// async function QueryParser(query) {
-//   const response = await chain.invoke({ query });
-//   return parser.parse(response.text);
-// }
+// For checking retrieved documents
 
-// const stream = await QueryParser.stream(
-//   // "What sort of rental revenue can i expect from renting a 1000 square foot laneway site in a lower value area, can you provide some samples based on general assumptions",
-//   // "How long does it take to get a building permit",
-//   // "Are laneway homes expensive?",
-//   "Are laneway homes a good investment?"
-//   // "What is the general opinion about modular laneway homes?",
-//   // "How long would it take me to get a building permit in Burnaby, BC?",
-// );
-
-// for await (const chunk of stream) {
-//   console.log(chunk);
-// }
+// const retrievedDocs = await vectorRetriever.getRelevantDocuments(query);
+// console.log({ retrievedDocs });
